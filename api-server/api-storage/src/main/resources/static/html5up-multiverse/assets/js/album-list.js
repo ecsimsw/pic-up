5b2d7594f4c265328290d97d112bfeff8fef67f3
ecsimsw <ecsimsw@gmail.com>
2023-09-29 00:05:50 +0900

    storage : add image dynamically

---
+++ api-server/api-storage/src/main/resources/static/html5up-multiverse/assets/js/album-list.js
@@ -0,0 +1,30 @@
+let albumMain = document.getElementById("album-main");
+let createBtn = document.getElementById("create-btn");
+
+let picNum = 0
+
+createBtn.addEventListener("click", function () {
+    picNum++;
+    const article = document.createElement('article');
+    article.id = `picture-${picNum}`
+    article.className = 'thumb'
+
+    const thumbImage = document.createElement('a');
+    thumbImage.className = "image"
+    thumbImage.id = "pic-1"
+    thumbImage.style.backgroundImage = "url('images/thumbs/01.jpg')"
+    thumbImage.style.cursor = "pointer"
+    thumbImage.style.outline = "0px"
+    thumbImage.innerHTML = `<img src="images/thumbs/0${picNum}.jpg" alt="" style="display: none;">`
+
+    const title = document.createElement('h2');
+    title.innerText = "Magna feugiat lorem"
+
+    const description = document.createElement('p');
+    description.innerText = "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit."
+
+    article.appendChild(thumbImage);
+    article.appendChild(title);
+    article.appendChild(description);
+    albumMain.appendChild(article);
+}, false);
