4b2b3d3cd98f709e8df2e087124fd4953d1cd237
ecsimsw <ecsimsw@gmail.com>
2023-09-29 01:00:29 +0900

    storage : remove unused module

---
+++ api-server/api-storage/src/main/resources/static/image-popup/index.js
@@ -0,0 +1,23 @@
+// $(document).ready(function(){
+//     $('.image-popup-vertical-fit').magnificPopup({
+//         type: 'image',
+//         mainClass: 'mfp-with-zoom',
+//         gallery:{
+//             enabled:true
+//         },
+//
+//         zoom: {
+//             enabled: true,
+//
+//             duration: 300, // duration of the effect, in milliseconds
+//             easing: 'ease-in-out', // CSS transition easing function
+//
+//             opener: function(openerElement) {
+//
+//                 return openerElement.is('img') ? openerElement : openerElement.find('img');
+//             }
+//         }
+//
+//     });
+//
+// });
