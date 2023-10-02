6ffdf2424d13f6a43734056c366cfd0e0bc7a5d0
ecsimsw <ecsimsw@gmail.com>
2023-10-02 15:37:40 +0900

    storage : add dynamic album image

--- api-server/api-storage/src/main/resources/static/html5up-multiverse/assets/js/album-list.js
+++ api-server/api-storage/src/main/resources/static/html5up-multiverse/assets/js/album-list.js
@@ -1,55 +1,52 @@
 let albumMain = document.getElementById("album-main");
 let createBtn = document.getElementById("create-btn");
 
-let picNum = 0
-
-document.getElementById('dynamic').addEventListener('click', function() {
-    lightGallery(document.getElementById('dynamic'), {
-        dynamic: true,
-        dynamicEl: [{
-            "src": 'images/fulls/01.jpg',
-            'thumb': 'images/thumbs/01.jpg',
-            'subHtml': '<h4>Fading Light</h4><p>Classic view from Rigwood Jetty on Coniston Water an old archive shot similar to an old post but a little later on.</p>'
-        }, {
-            'src': 'images/fulls/01.jpg',
-            'thumb': 'images/thumbs/01.jpg',
-            'subHtml': "<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>"
-        }, {
-            'src': 'images/fulls/01.jpg',
-            'thumb': 'images/thumbs/01.jpg',
-            'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
-        }]
-    })
-
-});
+let albumId = 0
 
 createBtn.addEventListener("click", function () {
-    picNum++;
-    createArticle(picNum);
+    albumId++;
+    createAlbumArticle(albumId);
+    addImageViewer(`album-${albumId}`);
 }, false);
 
-function createArticle() {
+function createAlbumArticle(albumId) {
     const article = document.createElement('article');
-    article.id = `picture-${picNum}`
+    article.id = `album-${albumId}`
     article.className = 'thumb'
 
     const thumbImage = document.createElement('a');
     thumbImage.className = "album-main-image"
-    thumbImage.href = "images/fulls/01.jpg"
-    thumbImage.id = "pic-1"
-    thumbImage.style.backgroundImage = "url('images/thumbs/01.jpg')"
+    thumbImage.id = `album-${albumId}-thumb`
+    const thumbImageResource = "images/thumbs/"+"0"+albumId+".jpg";
+    thumbImage.style.backgroundImage = "url('"+thumbImageResource+"')"
     thumbImage.style.cursor = "pointer"
     thumbImage.style.outline = "0px"
-    thumbImage.innerHTML = `<img src="images/thumbs/0${picNum}.jpg" alt="" style="display: none;">`
 
     const title = document.createElement('h2');
     title.innerText = "Magna feugiat lorem"
 
-    const description = document.createElement('p');
-    description.innerText = "Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit."
-
     article.appendChild(thumbImage);
     article.appendChild(title);
-    article.appendChild(description);
     albumMain.appendChild(article);
+}
+
+function addImageViewer(albumArticleId) {
+    document.getElementById(albumArticleId).addEventListener('click', function () {
+        lightGallery(document.getElementById(albumArticleId), {
+            dynamic: true,
+            dynamicEl: [{
+                "src": 'images/fulls/07.jpg',
+                'thumb': 'images/thumbs/01.jpg',
+                'subHtml': '<h4>Fading Light</h4><p>Classic view from Rigwood Jetty on Coniston Water an old archive shot similar to an old post but a little later on.</p>'
+            }, {
+                'src': 'images/fulls/08.jpg',
+                'thumb': 'images/thumbs/01.jpg',
+                'subHtml': "<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>"
+            }, {
+                'src': 'images/fulls/09.jpg',
+                'thumb': 'images/thumbs/01.jpg',
+                'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+            }]
+        })
+    });
 }
