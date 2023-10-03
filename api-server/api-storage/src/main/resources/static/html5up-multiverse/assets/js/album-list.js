6f480f21f477def9030dc7c51e93870f6f131d5b
ecsimsw <ecsimsw@gmail.com>
2023-10-03 21:54:36 +0900

    storage : creation panel

--- api-server/api-storage/src/main/resources/static/html5up-multiverse/assets/js/album-list.js
+++ api-server/api-storage/src/main/resources/static/html5up-multiverse/assets/js/album-list.js
@@ -1,8 +1,8 @@
 let albumMain = document.getElementById("album-main");
 let logoBtn = document.getElementById("logo");
-let settingBtn = document.getElementById("setting-btn");
+let editBtn = document.getElementById("edit-btn");
 
-let settingAble = false
+let editMode = false
 let albumId = 0
 
 logoBtn.addEventListener("click", function () {
@@ -11,11 +11,11 @@ logoBtn.addEventListener("click", function () {
     addImageViewer(`album-${albumId}`);
 }, false);
 
-settingBtn.addEventListener('click', function () {
-    settingAble = !settingAble
-    if(settingAble) {
-        settingBtn.style.backgroundColor = "#47c5ab"
-        settingBtn.style.color = "#ffffff"
+editBtn.addEventListener('click', function () {
+    editMode = !editMode
+    if(editMode) {
+        editBtn.style.backgroundColor = "#47c5ab"
+        editBtn.style.color = "#ffffff"
         $('#album-main').sortable({
             item: $('#album-main'),
             animation: 1000,
@@ -27,7 +27,7 @@ settingBtn.addEventListener('click', function () {
             }
         })
     } else {
-        settingBtn.style.backgroundColor = ""
+        editBtn.style.backgroundColor = ""
         $('#album-main').sortable("destroy")
     }
 })
@@ -82,5 +82,96 @@ function addImageViewer(albumArticleId) {
         document.getElementById('header').style.display = 'block'
     }, false);
 }
+
+let descriptionArea = document.getElementById("description");
+let imageBoxButton = document.getElementById("imageBoxButton");
+
+descriptionArea.addEventListener('input', function () {
+    let content = this.value;
+    let maxRows = 2;
+    const rows = content.split('\n').length;
+    if (rows > maxRows) {
+        this.value = content.slice(0, -1);
+    }
+}, false);
+
+imageBoxButton.addEventListener('change', function () {
+    let content = this.value
+    let filePath = content.split('\\');
+    let fileName = filePath[filePath.length - 1];
+    let imageBoxName = document.getElementById("imageBoxText");
+    imageBoxName.readOnly = false;
+    imageBoxName.value = fileName;
+    imageBoxName.readOnly = true;
+}, false);
+
+function initCreationPanel() {
+    const $body = $('body');
+    let $panels = $('.panel');
+
+    $panels.each(function () {
+        let $this = $(this),
+            $toggles = $('[href="#' + $this.attr('id') + '"]'),
+            $closer = $('<div class="closer" />').appendTo($this);
+
+        // Closer.
+        $closer.on('click', function (event) {
+            $this.trigger('---hide');
+        });
+
+        // Events.
+        $this.on('click', function (event) {
+            event.stopPropagation();
+        })
+
+        $this.on('---toggle', function () {
+
+            if ($this.hasClass('active')) {
+                $this.triggerHandler('---hide');
+            } else {
+                $this.triggerHandler('---show');
+            }
+
+        })
+
+        $this.on('---show', function () {
+
+            // Hide other content.
+            if ($body.hasClass('content-active')) {
+                $panels.trigger('---hide');
+            }
+
+            // Activate content, toggles.
+            $this.addClass('active');
+            $toggles.addClass('active');
+
+            // Activate body.
+            $body.addClass('content-active');
+
+        })
+
+        $this.on('---hide', function () {
+
+            // Deactivate content, toggles.
+            $this.removeClass('active');
+            $toggles.removeClass('active');
+
+            // Deactivate body.
+            $body.removeClass('content-active');
+
+        });
+
+        // Toggles.
+        $toggles.removeAttr('href')
+        .css('cursor', 'pointer')
+        .on('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            $this.trigger('---toggle');
+        });
+    });
+}
+
+initCreationPanel()
 
 
