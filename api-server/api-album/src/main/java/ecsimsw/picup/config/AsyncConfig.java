1f6cbb44fcf2d69d3a238fa686e371d87abea5f3
ecsimsw <ecsimsw@gmail.com>
2023-10-25 00:59:12 +0900

    album : set async thread pool

--- api-server/api-album/src/main/java/ecsimsw/picup/config/AsyncConfig.java
+++ api-server/api-album/src/main/java/ecsimsw/picup/config/AsyncConfig.java
@@ -2,6 +2,7 @@ package ecsimsw.picup.config;
 
 import ecsimsw.picup.logging.CustomLogger;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -15,13 +16,30 @@ public class AsyncConfig implements AsyncConfigurer {
 
     private static final CustomLogger LOGGER = CustomLogger.init(AsyncConfig.class);
 
+    private final int corePoolSize;
+    private final int maxPoolSize;
+    private final int queueCapacitySize;
+    private final String threadNamePrefix;
+
+    public AsyncConfig(
+        @Value("${async.thread.pool.core.pool.size}") int corePoolSize,
+        @Value("${async.thread.pool.max.pool.size}") int maxPoolSize,
+        @Value("${async.thread.pool.queue.capacity.size}") int queueCapacitySize,
+        @Value("${async.thread.pool.thread.name.prefix}") String threadNamePrefix
+    ) {
+        this.corePoolSize = corePoolSize;
+        this.maxPoolSize = maxPoolSize;
+        this.queueCapacitySize = queueCapacitySize;
+        this.threadNamePrefix = threadNamePrefix;
+    }
+
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(30);
-        executor.setQueueCapacity(50);
-        executor.setThreadNamePrefix("PICUP-ALBUM");
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacitySize);
+        executor.setThreadNamePrefix(threadNamePrefix);
         executor.initialize();
         return executor;
     }
