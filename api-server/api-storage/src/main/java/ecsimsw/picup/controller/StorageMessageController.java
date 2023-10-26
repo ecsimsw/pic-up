c414d4d40af4f1d29694bbcb5cbd1418099c68ba
ecsimsw <ecsimsw@gmail.com>
2023-10-27 03:46:35 +0900

    storage : rabbit mq dead letter queue

--- api-server/api-storage/src/main/java/ecsimsw/picup/controller/StorageMessageController.java
+++ api-server/api-storage/src/main/java/ecsimsw/picup/controller/StorageMessageController.java
@@ -4,8 +4,10 @@ import static ecsimsw.picup.config.RabbitMQContainerFactories.FILE_DELETION_QUEU
 
 import ecsimsw.picup.logging.CustomLogger;
 import ecsimsw.picup.service.StorageService;
+
 import java.util.List;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -20,12 +22,16 @@ public class StorageMessageController {
     }
 
     @RabbitListener(queues = "${mq.file.deletion.queue.name}", containerFactory = FILE_DELETION_QUEUE_CF)
-    public void deleteAll(List<String> resources) {
-        storageService.deleteAll(resources);
+    public void deleteAll(List<String> resources) throws InterruptedException {
+//        storageService.deleteAll(resources);
+
+            Thread.sleep(100000);
+            throw new IllegalArgumentException();
     }
 
-    @RabbitListener(queues = "${mq.file.deletion.recover.queue.name}", containerFactory = FILE_DELETION_QUEUE_CF)
-    public void deleteAllRecover(List<String> resources) {
-        LOGGER.error("dead letter from file deletion queue : " + String.join(", ", resources));
+    @RabbitListener(queues = "${mq.file.deletion.recover.queue.name}")
+    public void deleteAllRecover(Message failedMessage) {
+        LOGGER.error("dead letter from file deletion queue \n" +
+            "body : " + failedMessage.getPayload());
     }
 }
