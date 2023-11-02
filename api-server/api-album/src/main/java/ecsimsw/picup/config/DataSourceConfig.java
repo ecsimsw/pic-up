d8833dc748ed7ac8e7867b00cc5f425f7cf2a010
ecsimsw <ecsimsw@gmail.com>
2023-11-03 07:50:59 +0900

    album : replication config

--- api-server/api-album/src/main/java/ecsimsw/picup/config/DataSourceConfig.java
+++ api-server/api-album/src/main/java/ecsimsw/picup/config/DataSourceConfig.java
@@ -1,8 +1,10 @@
 package ecsimsw.picup.config;
 
+import static ecsimsw.picup.config.DBType.MASTER;
+import static ecsimsw.picup.config.DBType.SLAVE;
+
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,49 +13,58 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @Configuration
 public class DataSourceConfig {
 
-    @Bean(value = "MASTER")
+    private static final String DB_SOURCE_BEAN_ALIAS_MASTER = "MASTER_DB_SOURCE";
+    private static final String DB_SOURCE_BEAN_ALIAS_SLAVE = "SLAVE_DB_SOURCE";
+
+    @Bean(value = DB_SOURCE_BEAN_ALIAS_MASTER)
     @ConfigurationProperties(prefix = "spring.datasource.master")
     public DataSource masterDataSource() {
         return DataSourceBuilder.create().build();
     }
 
-    @Bean(value = "SLAVE")
+    @Bean(value = DB_SOURCE_BEAN_ALIAS_SLAVE)
     @ConfigurationProperties(prefix = "spring.datasource.slave")
     public DataSource slaveDataSource() {
         return DataSourceBuilder.create().build();
     }
 
     @Bean
-    public DataSource routingDataSource2(
-        @Qualifier(value = "MASTER") DataSource masterDataSource, // (1)
-        @Qualifier(value = "SLAVE") DataSource slaveDataSource
-    ) {
-        RoutingDataSource routingDataSource = new RoutingDataSource(); // (2)
-
-        HashMap<Object, Object> dataSourceMap = new HashMap<>(); // (3)
-        dataSourceMap.put("master", masterDataSource);
-        dataSourceMap.put("slave", slaveDataSource);
-
-        routingDataSource.setTargetDataSources(dataSourceMap); // (4)
-        routingDataSource.setDefaultTargetDataSource(masterDataSource); // (5)
-
-        return routingDataSource;
-    }
-
-    @Bean
     @Primary
-    public DataSource dataSource() {
-        DataSource determinedDataSource = routingDataSource2(
-            masterDataSource(),
-            slaveDataSource()
+    public DataSource dataSource(
+        @Qualifier(DB_SOURCE_BEAN_ALIAS_MASTER) DataSource masterDataSource,
+        @Qualifier(DB_SOURCE_BEAN_ALIAS_SLAVE) DataSource slaveDataSource
+    ) {
+        var routingDataSource = new RoutingDataSource();
+        routingDataSource.setDefaultTargetDataSource(masterDataSource);
+        routingDataSource.setTargetDataSources(
+            Map.of(
+                MASTER, masterDataSource,
+                SLAVE, slaveDataSource
+            )
         );
-        return new LazyConnectionDataSourceProxy(determinedDataSource);
+        return new LazyConnectionDataSourceProxy(routingDataSource);
     }
+}
 
+class RoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+        if (isReadOnly) {
+            return SLAVE;
+        }
+        return MASTER;
+    }
+}
+
+enum DBType {
+    MASTER,
+    SLAVE
 }
--- api-server/api-storage/src/main/java/ecsimsw/picup/PicUpStorageApplication.java
+++ api-server/api-storage/src/main/java/ecsimsw/picup/PicUpStorageApplication.java
@@ -1,12 +1,7 @@
 package ecsimsw.picup;
 
-import ecsimsw.picup.domain.Resource;
-import ecsimsw.picup.domain.ResourceRepository;
-import java.nio.charset.StandardCharsets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.mock.web.MockMultipartFile;
 
 @SpringBootApplication
 public class PicUpStorageApplication {
@@ -14,16 +9,6 @@ public class PicUpStorageApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(PicUpStorageApplication.class);
         app.setAdditionalProfiles("dev");
-        ConfigurableApplicationContext run = app.run(args);
-
-        ResourceRepository bean = run.getBean(ResourceRepository.class);
-        var resource = bean.save(Resource.createRequested("tag", new MockMultipartFile("hi.jpg", "string".getBytes(StandardCharsets.UTF_8))));
-        var saved = bean.findById(resource.getResourceKey()).orElseThrow();
-
-        var saved2 = bean.save(new Resource(saved.getResourceKey(), saved.getStoredStorages(), null, saved.getDeleteRequested()));
-        System.out.println("fff " + saved.getCreateRequested());
-        System.out.println("fff " + saved2.getCreateRequested());
-
-
+        app.run(args);
     }
 }
