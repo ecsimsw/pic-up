b0681db74769a8bd4c03d8546c77a2ca569e101e
ecsimsw <ecsimsw@gmail.com>
2023-11-16 10:57:25 +0900

    infra : IAC cloud server with terraform

---
+++ infra/server-cloud/sg/main.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "myCloudSg" {
+  name        = "myCloud-sg"
+  description = "Allow access"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["183.100.1.179/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
