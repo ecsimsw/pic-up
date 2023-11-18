90afdab8bb3e546fe6ff8861786012cf511711f4
ecsimsw <ecsimsw@gmail.com>
2023-11-18 18:48:43 +0900

    test : update test script

--- infra/stress-test/script.js
+++ infra/stress-test/script.js
@@ -2,12 +2,29 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 10,
+    vus: 1000,
     duration: '30s'
 };
 
 export default function () {
-    const res = http.get('http://localhost:8080/api');
+    let url = "http://picup.ecsimsw.com:52080/api/member/signin";
+    let data = JSON.stringify({
+        username : "nhdi",
+        password : "passwrod"
+    });
+    let params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'dataType': 'json'
+        }
+    };
+
+    let res = http.post(url, data, params);
+
+    url = "http://picup.ecsimsw.com:52080/api/storage/1-fc59773c-0774-480e-8117-18f714360a4b.png"
+    res = http.get(url, params);
     check(res, { 'status was 200': (r) => r.status == 200 });
     sleep(1);
 }
+
+// docker run --rm -i grafana/k6 run - <script.js
