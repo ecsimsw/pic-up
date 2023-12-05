7aa44afd1d2b48ba5b3c142db10fa5ea64f09fb7
ecsimsw <ecsimsw@gmail.com>
2023-12-06 00:44:49 +0900

    release : 1.8.0

--- server-api/api-member/src/main/java/ecsimsw/picup/PicUpMemberApplication.java
+++ server-api/api-member/src/main/java/ecsimsw/picup/PicUpMemberApplication.java
@@ -2,14 +2,12 @@ package ecsimsw.picup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 public class PicUpMemberApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(PicUpMemberApplication.class);
-        app.setAdditionalProfiles("dev");
         app.run(args);
     }
 }
