b90e4dc3c0c74a8fb99d13b891b9eab80669418b
ecsimsw <ecsimsw@gmail.com>
2023-12-06 02:35:40 +0900

    monitoring : add prod env

--- infra-kubernetes/port-forward.sh
+++ infra-kubernetes/port-forward.sh
@@ -2,6 +2,6 @@
 kubectl port-forward -n kong service/kong-gateway-proxy 52080:80 --address 0.0.0.0
 
 # picup services only for inner networks / NEED TO BE CHANGED ALLOW ADDRESS
-kubectl port-forward -n picup service/member-server-svc 528082:8082 --address 0.0.0.0
-kubectl port-forward -n picup service/storage-server-svc 528083:8083 --address 0.0.0.0
-kubectl port-forward -n picup service/album-server-svc 528084:8084 --address 0.0.0.0
+kubectl port-forward -n picup service/member-server-svc 52082:8082 --address 127.0.0.1 & \
+kubectl port-forward -n picup service/storage-server-svc 52083:8083 --address 127.0.0.1 & \
+kubectl port-forward -n picup service/album-server-svc 52084:8084 --address 127.0.0.1 &
