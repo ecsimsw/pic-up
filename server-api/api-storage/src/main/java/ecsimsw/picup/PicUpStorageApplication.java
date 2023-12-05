f0d26852e08e8698e7dfa8d9358061f13497b599
ecsimsw <ecsimsw@gmail.com>
2023-12-06 01:25:10 +0900

    release : 1.10.0

--- server-api/api-storage/src/main/java/ecsimsw/picup/PicUpStorageApplication.java
+++ server-api/api-storage/src/main/java/ecsimsw/picup/PicUpStorageApplication.java
@@ -1,8 +1,5 @@
 package ecsimsw.picup;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +8,6 @@ public class PicUpStorageApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(PicUpStorageApplication.class);
-        app.setAdditionalProfiles("dev");
         app.run(args);
     }
 }
