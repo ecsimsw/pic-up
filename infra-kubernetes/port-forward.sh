db8a46ccbadba940aa3178ea51ed3855c85778ce
ecsimsw <ecsimsw@gmail.com>
2023-12-11 22:42:01 +0900

    infra-kubernetes : add prometheus target

--- infra-kubernetes/port-forward.sh
+++ infra-kubernetes/port-forward.sh
@@ -6,4 +6,7 @@ nohup kubectl port-forward -n kong service/kong-gateway-proxy 52080:80 --address
 nohup kubectl port-forward -n picup service/member-server-svc 52082:8082 --address 0.0.0.0 & \
 nohup kubectl port-forward -n picup service/storage-server-svc 52083:8083 --address 0.0.0.0 & \
 nohup kubectl port-forward -n picup service/album-server-svc 52084:8084 --address 0.0.0.0 & \
-nohup kubectl port-forward -n monitoring service/node-exporter-http 52910:9100 --address 0.0.0.0 &
+nohup kubectl port-forward -n monitoring service/kube-state-metrics 52090:8090 --address 0.0.0.0 & \
+nohup kubectl port-forward -n monitoring service/node-exporter-http 52091:9100 --address 0.0.0.0 &
+
+# ps -ef | grep "kubectl port"
