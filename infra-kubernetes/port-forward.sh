2bab42425fbbf88ce42e1100a8d3449945a6655e
ecsimsw <ecsimsw@gmail.com>
2023-12-12 03:26:58 +0900

    utils-monitoring : add docker container metrics

--- infra-kubernetes/port-forward.sh
+++ infra-kubernetes/port-forward.sh
@@ -1,3 +1,5 @@
+# ps -ef | grep "kubectl port"
+
 # kong ingress proxy
 nohup kubectl port-forward -n kong service/kong-gateway-proxy 52080:80 --address 0.0.0.0 &
 
@@ -8,5 +10,3 @@ nohup kubectl port-forward -n picup service/storage-server-svc 52083:8083 --addr
 nohup kubectl port-forward -n picup service/album-server-svc 52084:8084 --address 0.0.0.0 & \
 nohup kubectl port-forward -n monitoring service/kube-state-metrics 52090:8090 --address 0.0.0.0 & \
 nohup kubectl port-forward -n monitoring service/node-exporter-http 52091:9100 --address 0.0.0.0 &
-
-# ps -ef | grep "kubectl port"
