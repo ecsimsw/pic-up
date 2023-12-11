3477de792d94afb0328540c5ee151ec2dd8a7a91
ecsimsw <ecsimsw@gmail.com>
2023-12-11 22:14:25 +0900

    infra-kubernetes : pf and prometheus target for node exporter

--- infra-kubernetes/port-forward.sh
+++ infra-kubernetes/port-forward.sh
@@ -5,4 +5,5 @@ nohup kubectl port-forward -n kong service/kong-gateway-proxy 52080:80 --address
 # From now, this is allow all the address. This should not be localhost cause docker internal address
 nohup kubectl port-forward -n picup service/member-server-svc 52082:8082 --address 0.0.0.0 & \
 nohup kubectl port-forward -n picup service/storage-server-svc 52083:8083 --address 0.0.0.0 & \
-nohup kubectl port-forward -n picup service/album-server-svc 52084:8084 --address 0.0.0.0 &
+nohup kubectl port-forward -n picup service/album-server-svc 52084:8084 --address 0.0.0.0 & \
+nohup kubectl port-forward -n monitoring service/node-exporter-http 52910:9100 --address 0.0.0.0 &
