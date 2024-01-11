8dffb0ebc1dc70084bbf811c1e3c536b49dcb724
ecsimsw <ecsimsw@gmail.com>
2024-01-12 08:38:45 +0900

    Lock in select query

--- server-api/api-album/src/main/java/ecsimsw/picup/domain/StorageUsageRepository.java
+++ server-api/api-album/src/main/java/ecsimsw/picup/domain/StorageUsageRepository.java
@@ -1,10 +1,13 @@
 package ecsimsw.picup.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 public interface StorageUsageRepository extends JpaRepository<StorageUsage, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<StorageUsage> findByUserId(Long userId);
 }
