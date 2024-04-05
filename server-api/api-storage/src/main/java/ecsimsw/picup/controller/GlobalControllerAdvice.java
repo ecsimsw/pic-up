949f6340e4c3b3d3dabd0efee1609a8faa463133
ecsimsw <ecsimsw@gmail.com>
2024-04-05 20:38:38 +0900

    prod properties shell script

--- server-api/api-storage/src/main/java/ecsimsw/picup/controller/GlobalControllerAdvice.java
+++ server-api/api-storage/src/main/java/ecsimsw/picup/controller/GlobalControllerAdvice.java
@@ -1,54 +1,54 @@
-package ecsimsw.picup.controller;
-
-import ecsimsw.picup.alert.SlackMessageSender;
-import ecsimsw.picup.auth.UnauthorizedException;
-import ecsimsw.picup.exception.InvalidResourceException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
-
-import javax.validation.ConstraintViolationException;
-
-@ControllerAdvice
-public class GlobalControllerAdvice {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalControllerAdvice.class);
-
-    @ExceptionHandler(InvalidResourceException.class)
-    public ResponseEntity<String> fileIOException(InvalidResourceException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
-    @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<String> fileSizeException(MaxUploadSizeExceededException e) {
-        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(e.getMessage());
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<String> invalidRequestParameter(ConstraintViolationException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid format in request data");
-    }
-
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<String> wrongRequestMethod(HttpRequestMethodNotSupportedException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
-    @ExceptionHandler({UnauthorizedException.class})
-    public ResponseEntity<String> unauthorizedException() {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("unauthorized user request");
-    }
-
-    @ExceptionHandler(Throwable.class)
-    public ResponseEntity<String> unhandledException(Throwable e) {
-        var alertMessage = "[UNHANDLED] : " + e.getMessage();
-        LOGGER.error(alertMessage);
-        SlackMessageSender.send(alertMessage);
-        return ResponseEntity.internalServerError().body("unhandled server exception");
-    }
-}
+//package ecsimsw.picup.controller;
+//
+//import ecsimsw.picup.alert.SlackMessageSender;
+//import ecsimsw.picup.auth.UnauthorizedException;
+//import ecsimsw.picup.exception.InvalidResourceException;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.HttpRequestMethodNotSupportedException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.multipart.MaxUploadSizeExceededException;
+//
+//import javax.validation.ConstraintViolationException;
+//
+//@ControllerAdvice
+//public class GlobalControllerAdvice {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalControllerAdvice.class);
+//
+//    @ExceptionHandler(InvalidResourceException.class)
+//    public ResponseEntity<String> fileIOException(InvalidResourceException e) {
+//        return ResponseEntity.badRequest().body(e.getMessage());
+//    }
+//
+//    @ExceptionHandler(MaxUploadSizeExceededException.class)
+//    public ResponseEntity<String> fileSizeException(MaxUploadSizeExceededException e) {
+//        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(e.getMessage());
+//    }
+//
+//    @ExceptionHandler(ConstraintViolationException.class)
+//    public ResponseEntity<String> invalidRequestParameter(ConstraintViolationException e) {
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid format in request data");
+//    }
+//
+//    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+//    public ResponseEntity<String> wrongRequestMethod(HttpRequestMethodNotSupportedException e) {
+//        return ResponseEntity.badRequest().body(e.getMessage());
+//    }
+//
+//    @ExceptionHandler({UnauthorizedException.class})
+//    public ResponseEntity<String> unauthorizedException() {
+//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("unauthorized user request");
+//    }
+//
+//    @ExceptionHandler(Throwable.class)
+//    public ResponseEntity<String> unhandledException(Throwable e) {
+//        var alertMessage = "[UNHANDLED] : " + e.getMessage();
+//        LOGGER.error(alertMessage);
+//        SlackMessageSender.send(alertMessage);
+//        return ResponseEntity.internalServerError().body("unhandled server exception");
+//    }
+//}
