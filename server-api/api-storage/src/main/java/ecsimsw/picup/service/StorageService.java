f4b06b9f5549849802ec1f38344f5cab0fd69972
ecsimsw <ecsimsw@gmail.com>
2024-04-05 20:43:05 +0900

    prod properties shell script

--- server-api/api-storage/src/main/java/ecsimsw/picup/service/StorageService.java
+++ server-api/api-storage/src/main/java/ecsimsw/picup/service/StorageService.java
@@ -55,6 +55,7 @@ public class StorageService {
                     backUpStorage.deleteIfExists(resourceKey);
                 })
             );
+            e.printStackTrace();
             throw new StorageException("exception while uploading : " + e.getMessage());
         }
     }
