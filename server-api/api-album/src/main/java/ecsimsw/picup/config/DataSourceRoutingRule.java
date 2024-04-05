1bc372c84d6247821b1c7d085d3493c2f204680f
ecsimsw <ecsimsw@gmail.com>
2024-04-05 20:32:40 +0900

    prod properties shell script

--- server-api/api-album/src/main/java/ecsimsw/picup/config/DataSourceRoutingRule.java
+++ server-api/api-album/src/main/java/ecsimsw/picup/config/DataSourceRoutingRule.java
@@ -8,16 +8,16 @@ public class DataSourceRoutingRule extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
-        if (DataSourceTargetContextHolder.hasDirectTargetDataSource()) {
-            return DataSourceTargetContextHolder.getTargetContext();
-        }
-        var isReadOnlyQuery = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
-        if (isReadOnlyQuery && !DataSourceHealth.isDown(DataSourceType.SLAVE)) {
-            return DataSourceType.SLAVE;
-        }
-        if (!isReadOnlyQuery && DataSourceHealth.isDown(DataSourceType.MASTER)) {
-            throw new DataSourceConnectionDownException("Server can read only now");
-        }
+//        if (DataSourceTargetContextHolder.hasDirectTargetDataSource()) {
+//            return DataSourceTargetContextHolder.getTargetContext();
+//        }
+//        var isReadOnlyQuery = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+//        if (isReadOnlyQuery && !DataSourceHealth.isDown(DataSourceType.SLAVE)) {
+//            return DataSourceType.SLAVE;
+//        }
+//        if (!isReadOnlyQuery && DataSourceHealth.isDown(DataSourceType.MASTER)) {
+//            throw new DataSourceConnectionDownException("Server can read only now");
+//        }
         return DataSourceType.MASTER;
     }
 }
