18489cdbea523c199d0774f058e27dda56e2778d
ecsimsw <ecsimsw@gmail.com>
2024-04-10 20:41:38 +0900

    migrate data (fs -> s3)

--- server-api/src/main/java/ecsimsw/picup/PicUpAlbumApplication.java
+++ server-api/src/main/java/ecsimsw/picup/PicUpAlbumApplication.java
@@ -1,9 +1,12 @@
 package ecsimsw.picup;
 
+import ecsimsw.picup.album.domain.PictureFileExtension;
+import ecsimsw.picup.album.dto.FileUploadResponse;
 import ecsimsw.picup.album.dto.SignUpRequest;
 import ecsimsw.picup.album.service.FileDeletionScheduler;
 import ecsimsw.picup.album.service.FileStorageService;
 import ecsimsw.picup.album.service.MemberService;
+import ecsimsw.picup.storage.service.ObjectStorage;
 import ecsimsw.picup.storage.utils.FileUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,26 +14,40 @@ import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @EnableScheduling
 @EnableRetry
 @SpringBootApplication
 public class PicUpAlbumApplication {
 
-    public static void main(String[] args) {
-//        var app = new SpringApplication(PicUpAlbumApplication.class);
-//        app.setAdditionalProfiles("prod");
-
-//        var ctx = app.run(args);
+    public static void main(String[] args) throws IOException {
+        var app = new SpringApplication(PicUpAlbumApplication.class);
+        app.setAdditionalProfiles("prod");
+//
+        var ctx = app.run(args);
 //        var outboxService = ctx.getBean(FileDeletionScheduler.class);
 //        outboxService.schedulePublishOut();
 
 //        FileStorageService bean = ctx.getBean(FileStorageService.class);
 //        FileUtils.read("./storage-backup/");
 
+        ObjectStorage bean = ctx.getBean(ObjectStorage.class);
+
         File dir = new File("./storage-backup/");
-        String files[] = dir.list();
-        for(String f : files) {
+        for(File f : dir.listFiles()) {
+            System.out.println("upload : "+ f.getName());
+            byte[] byteFile = Files.readAllBytes(f.toPath());
+            bean.storeAsync(
+                f.getName(),
+                new FileUploadResponse(
+                    f.getName(),
+                    PictureFileExtension.fromFileName(f.getName()),
+                    byteFile.length,
+                    byteFile
+                    )
+            );
             System.out.println(f);
         }
     }
