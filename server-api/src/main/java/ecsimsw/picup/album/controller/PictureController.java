2194010d55e9dc609157d22449264d980b6ee812
ecsimsw <ecsimsw@gmail.com>
2024-04-16 06:44:41 +0900

    temp test api

--- server-api/src/main/java/ecsimsw/picup/album/controller/PictureController.java
+++ server-api/src/main/java/ecsimsw/picup/album/controller/PictureController.java
@@ -46,9 +46,7 @@ public class PictureController {
         @PathVariable Long albumId,
         @RequestPart MultipartFile file
     ) {
-        var start = System.currentTimeMillis();
         var pictureId = pictureUploadService.upload(1l, albumId, file);
-        log.info("upload end : " + (System.currentTimeMillis() - start) + "ms");
         return ResponseEntity.ok(pictureId);
     }
 
--- server-api/src/main/java/ecsimsw/picup/album/service/PictureUploadService.java
+++ server-api/src/main/java/ecsimsw/picup/album/service/PictureUploadService.java
@@ -7,9 +7,11 @@ import ecsimsw.picup.album.dto.PictureInfoResponse;
 import ecsimsw.picup.storage.dto.VideoFileUploadResponse;
 import ecsimsw.picup.album.utils.UserLock;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PictureUploadService {
@@ -21,14 +23,17 @@ public class PictureUploadService {
     private final PictureService pictureService;
 
     public Long upload(Long userId, Long albumId, MultipartFile file) {
+        var start = System.currentTimeMillis();
         if (PictureFileExtension.of(file).isVideo) {
             var videoFile = fileService.uploadVideo(FileUploadRequest.of(file));
             var pictureInfo = uploadVideo(userId, albumId, videoFile);
+            log.info("upload end : " + (System.currentTimeMillis() - start) + "ms, " + videoFile.resourceKey());
             return pictureInfo.id();
         }
         var imageFile = fileService.uploadImage(FileUploadRequest.of(file));
         var thumbnailFile = fileService.uploadImage(FileUploadRequest.resizedOf(file, PICTURE_THUMBNAIL_SCALE));
         var pictureInfo = uploadImage(userId, albumId, imageFile, thumbnailFile);
+        log.info("upload end : " + (System.currentTimeMillis() - start) + "ms, " + imageFile.resourceKey());
         return pictureInfo.id();
     }
 
