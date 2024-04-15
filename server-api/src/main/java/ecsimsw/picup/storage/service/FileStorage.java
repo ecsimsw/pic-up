7c0b409ae3df69d2b94c735d9e0737d6ea9cf8e9
ecsimsw <ecsimsw@gmail.com>
2024-04-16 07:02:29 +0900

    temp test api

--- server-api/src/main/java/ecsimsw/picup/storage/service/FileStorage.java
+++ server-api/src/main/java/ecsimsw/picup/storage/service/FileStorage.java
@@ -21,7 +21,7 @@ public class FileStorage {
         try {
             var start = System.currentTimeMillis();
             file.transferTo(new File(FILE_STORAGE_PATH + resourceKey));
-            log.info("fs upload time " + (System.currentTimeMillis() - start) + "ms");
+//            log.info("fs upload time " + (System.currentTimeMillis() - start) + "ms");
             return new AsyncResult<>(resourceKey).completable();
         } catch (IOException e) {
             throw new StorageException("Failed to upload to file storage");
