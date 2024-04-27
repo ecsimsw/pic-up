5ce05cd7d2563f90d030e0c1f7e7d1dd4a60a995
ecsimsw <ecsimsw@gmail.com>
2024-04-27 12:59:08 +0900

    add loki

--- server-api/src/main/java/ecsimsw/picup/storage/S3Utils.java
+++ server-api/src/main/java/ecsimsw/picup/storage/S3Utils.java
@@ -3,14 +3,12 @@ package ecsimsw.picup.storage;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import ecsimsw.picup.album.exception.StorageException;
+import java.text.DecimalFormat;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 public class S3Utils {
-
-    private static final long SLOW_UPLOAD_THRESHOLD = 30_000;
 
     public static void store(AmazonS3 s3Client, String bucket, String path, MultipartFile file) {
         try {
@@ -19,9 +17,7 @@ public class S3Utils {
             metadata.setContentType(file.getContentType());
             metadata.setContentLength(file.getSize());
             s3Client.putObject(bucket, path, file.getInputStream(), metadata);
-            if(SLOW_UPLOAD_THRESHOLD < (System.currentTimeMillis() - start)) {
-                log.warn("Slow S3 upload time : " + (System.currentTimeMillis() - start));
-            }
+            log.info("S3 upload time : " + (System.currentTimeMillis() - start) + " ms, " + fileStringSize(file.getSize()));
         } catch (Exception e) {
             throw new StorageException("Object storage server exception while uploading", e);
         }
@@ -31,6 +27,22 @@ public class S3Utils {
         if (s3Client.doesObjectExist(bucket, path)) {
             s3Client.deleteObject(bucket, path);
         }
+    }
+
+    private static String fileStringSize(long size) {
+        var df = new DecimalFormat("0.00");
+        var sizeKb = 1024.0f;
+        var sizeMb = sizeKb * sizeKb;
+        var sizeGb = sizeMb * sizeKb;
+        var sizeTerra = sizeGb * sizeKb;
+        if (size < sizeMb) {
+            return df.format(size / sizeKb) + " Kb";
+        } else if (size < sizeGb) {
+            return df.format(size / sizeMb) + " Mb";
+        } else if (size < sizeTerra) {
+            return df.format(size / sizeGb) + " Gb";
+        }
+        return size + "b";
     }
 }
 
