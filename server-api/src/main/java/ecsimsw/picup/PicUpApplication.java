a64eca7d4d340eb6f91a585fabb10a0e488a91c5
ecsimsw <ecsimsw@gmail.com>
2024-04-28 14:45:14 +0900

    update max pool size

--- server-api/src/main/java/ecsimsw/picup/PicUpApplication.java
+++ server-api/src/main/java/ecsimsw/picup/PicUpApplication.java
@@ -1,11 +1,14 @@
 package ecsimsw.picup;
 
+import ecsimsw.picup.album.dto.SignUpRequest;
+import ecsimsw.picup.album.service.MemberService;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -18,6 +21,11 @@ public class PicUpApplication {
     public static void main(String[] args) {
         var app = new SpringApplication(PicUpApplication.class);
         app.setAdditionalProfiles("prod");
-        app.run(args);
+        ConfigurableApplicationContext run = app.run(args);
+        MemberService bean = run.getBean(MemberService.class);
+
+        for(int i = 33; i< 100; i++) {
+            bean.signUp(new SignUpRequest("ecsimsw" + i, "password"));
+        }
     }
 }
