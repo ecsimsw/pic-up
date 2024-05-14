27174f29772ddafa8ba9ffa036d3af1807809321
ecsimsw <ecsimsw@gmail.com>
2024-05-15 01:53:55 +0900

    fix remote ip resolver

--- server-album/src/test/java/ecsimsw/picup/presentation/MemberControllerTest.java
+++ server-album/src/test/java/ecsimsw/picup/presentation/MemberControllerTest.java
@@ -21,6 +21,7 @@ import ecsimsw.picup.album.service.MemberService;
 import ecsimsw.picup.auth.AuthArgumentResolver;
 import ecsimsw.picup.auth.AuthInterceptor;
 import ecsimsw.picup.auth.AuthTokenService;
+import ecsimsw.picup.auth.AuthTokens;
 import ecsimsw.picup.auth.UnauthorizedException;
 import javax.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayName;
@@ -51,25 +52,30 @@ class MemberControllerTest {
     @Test
     public void signInTest() throws Exception {
         var signInRequest = new SignInRequest("username", "password");
-        var expectBody = new MemberResponse(1L, signInRequest.username(), 0L, 0L);
-        var expectedAt = new Cookie(ACCESS_TOKEN_COOKIE_NAME, "accessToken");
-        var expectedRt = new Cookie(REFRESH_TOKEN_COOKIE_NAME, "refreshToken");
+        var memberResponse = new MemberResponse(1L, signInRequest.username(), 0L, 0L);
+        var accessToken = "accessToken";
+        var refreshToken = "refreshToken";
+        var loginAuthToken = AuthTokens.of(memberResponse.toTokenPayload(), accessToken, refreshToken);
 
         when(memberService.signIn(signInRequest))
-            .thenReturn(expectBody);
-        when(authTokenService.accessTokenCookie(any()))
-            .thenReturn(expectedAt);
+            .thenReturn(memberResponse);
+        when(authTokenService.issue(memberResponse.toTokenPayload()))
+            .thenReturn(loginAuthToken);
+        when(authTokenService.accessTokenCookie(loginAuthToken))
+            .thenReturn(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken));
         when(authTokenService.refreshTokenCookie(any()))
-            .thenReturn(expectedRt);
+            .thenReturn(new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken));
 
         mockMvc.perform(post("/api/member/signin")
+                .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken))
+                .cookie(new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken))
                 .content(OBJECT_MAPPER.writeValueAsString(signInRequest))
                 .contentType(MediaType.APPLICATION_JSON)
             )
             .andExpect(status().isOk())
-            .andExpect(cookie().value(expectedAt.getName(), expectedAt.getValue()))
-            .andExpect(cookie().value(expectedRt.getName(), expectedRt.getValue()))
-            .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(expectBody)));
+            .andExpect(cookie().value(ACCESS_TOKEN_COOKIE_NAME, accessToken))
+            .andExpect(cookie().value(REFRESH_TOKEN_COOKIE_NAME, refreshToken))
+            .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(memberResponse)));
     }
 
     @DisplayName("로그인 실패시 403 에러를 응답한다.")
@@ -93,25 +99,28 @@ class MemberControllerTest {
     @Test
     public void signUp() throws Exception {
         var signUpRequest = new SignUpRequest("username", "password");
-        var expectBody = new MemberResponse(1L, signUpRequest.username(), 0L, 0L);
-        var expectedAt = new Cookie(ACCESS_TOKEN_COOKIE_NAME, "accessToken");
-        var expectedRt = new Cookie(REFRESH_TOKEN_COOKIE_NAME, "refreshToken");
+        var memberResponse = new MemberResponse(1L, signUpRequest.username(), 0L, 0L);
+        var accessToken = "accessToken";
+        var refreshToken = "refreshToken";
+        var loginAuthToken = AuthTokens.of(memberResponse.toTokenPayload(), accessToken, refreshToken);
 
         when(memberService.signUp(signUpRequest))
-            .thenReturn(expectBody);
-        when(authTokenService.accessTokenCookie(any()))
-            .thenReturn(expectedAt);
-        when(authTokenService.refreshTokenCookie(any()))
-            .thenReturn(expectedRt);
+            .thenReturn(memberResponse);
+        when(authTokenService.issue(memberResponse.toTokenPayload()))
+            .thenReturn(loginAuthToken);
+        when(authTokenService.accessTokenCookie(loginAuthToken))
+            .thenReturn(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken));
+        when(authTokenService.refreshTokenCookie(loginAuthToken))
+            .thenReturn(new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken));
 
         mockMvc.perform(post("/api/member/signup")
                 .content(OBJECT_MAPPER.writeValueAsString(signUpRequest))
                 .contentType(MediaType.APPLICATION_JSON)
             )
             .andExpect(status().isOk())
-            .andExpect(cookie().value(expectedAt.getName(), expectedAt.getValue()))
-            .andExpect(cookie().value(expectedRt.getName(), expectedRt.getValue()))
-            .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(expectBody)));
+            .andExpect(cookie().value(ACCESS_TOKEN_COOKIE_NAME, accessToken))
+            .andExpect(cookie().value(REFRESH_TOKEN_COOKIE_NAME, refreshToken))
+            .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(memberResponse)));
     }
 
     @DisplayName("사용자 이름을 빈칸으로 회원정보로 가입할 수 없다.")
